7e451e6f4f70624214b4333098ab511212e6b1f3
mike-data-nexus <112513079+mike-data-nexus@users.noreply.github.com>
2023-04-25 13:45:43 -0400

    Updated schema, yaml, proxy

--- src/lens-hub-proxy.ts
+++ src/lens-hub-proxy.ts
@@ -1,46 +1,55 @@
 import {
+  ProfileCreated,
+  PostCreated,
+  MirrorCreated,
+} from "./../generated/LensHub/LensHub";
+import {
   AdminChanged as AdminChangedEvent,
   BeaconUpgraded as BeaconUpgradedEvent,
-  Upgraded as UpgradedEvent
-} from "../generated/LensHubProxy/LensHubProxy"
-import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/schema"
+  Upgraded as UpgradedEvent,
+} from "../generated/LensHubProxy/LensHubProxy";
+import { Profile, Post, Mirror } from "../generated/schema";
+import { BigInt } from "@graphprotocol/graph-ts";
 
-export function handleAdminChanged(event: AdminChangedEvent): void {
-  let entity = new AdminChanged(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+export function handleProfileCreated(event: ProfileCreated): void {
+  let profile = new Profile(event.params.profileId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  profile.creator = event.params.creator;
+  profile.to = event.params.to;
+  profile.handle = event.params.handle;
+  profile.imageURI = event.params.imageURI;
+  profile.followModule = event.params.followModule.toHexString();
+  profile.followModuleReturnData = event.params.followModuleReturnData;
+  profile.followNFTURI = event.params.followNFTURI;
+  profile.timestamp = event.params.timestamp;
 
-  entity.save()
+  profile.save();
 }
 
-export function handleBeaconUpgraded(event: BeaconUpgradedEvent): void {
-  let entity = new BeaconUpgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.beacon = event.params.beacon
+export function handlePostCreated(event: PostCreated): void {
+  let post = new Post(event.params.pubId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  post.profileId = event.params.profileId.toHexString();
+  post.contentURI = event.params.contentURI;
+  post.collectModule = event.params.collectModule;
+  post.collectModuleReturnData = event.params.collectModuleReturnData;
+  post.refernceModule = event.params.referenceModule;
+  post.refenceModuleReturnData = event.params.referenceModuleReturnData;
+  post.timestamp = event.params.timestamp;
 
-  entity.save()
+  post.save();
 }
 
-export function handleUpgraded(event: UpgradedEvent): void {
-  let entity = new Upgraded(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.implementation = event.params.implementation
+export function handleMirrorCreated(event: MirrorCreated): void {
+  let mirror = new Mirror(event.params.pubId.toString());
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  mirror.profileId = event.params.profileId.toHexString();
+  mirror.profileIdPointed = event.params.profileIdPointed;
+  mirror.pubIdPointed = event.params.pubIdPointed;
+  mirror.referenceModuleData = event.params.referenceModuleData;
+  mirror.referenceModule = event.params.referenceModule;
+  mirror.referenceModuleReturnData = event.params.referenceModuleReturnData;
+  mirror.timestamp = event.params.timestamp;
 
-  entity.save()
+  mirror.save();
 }
